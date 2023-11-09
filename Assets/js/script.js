859caaed92a09ce3561d21c5d09266f88a69f391
Fabiano <artheus07@gmail.com>
2023-11-09 04:28:02 -0300

    Atualizacao basica

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -19,7 +19,7 @@ function draw() {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.05)';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    ctx.fillStyle = '#0F0';
+    ctx.fillStyle = '#7d797a';  /* Color matrix "#0F0" effect */
     ctx.font = `${fontSize}px arial`;
 
     for (let i = 0; i < drops.length; i++) {
