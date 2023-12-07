86c7c5ce6d2a2cf2ab28dd30e8c4833f4b97f5d5
Artheus <artheus07@gmail.com>
2023-12-07 16:43:13 -0300

    validando checkbox e campos de dados

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -1,5 +1,17 @@
 function redirectToWifiPage() {
-  window.location.href = "wifi-publico.html";
+  var username = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var checkBox = document.getElementById("terms-checkbox");
+  
+  
+  if (username.trim() === "" || password.trim() === "") {
+    alert("Por favor, preencha os campos de login e senha.");
+    
+  } else if (!checkBox.checked) {
+    alert("Por favor, concorde com os termo de responsabilidade para prosseguir.");
+  } else {
+    window.location.href = "wifi-publico.html";
+  }
 }
 
 document.getElementById("login-button").addEventListener("click", redirectToWifiPage);
